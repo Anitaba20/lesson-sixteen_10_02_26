c82a980000ae214f55144d709923e89b25e7fa07
anita <anita.bacova@gmail.com>
2026-02-14 15:30:42 +0100

    type of integers_pickle

--- 01 list of integers_pickle.py
+++ 01 list of integers_pickle.py
@@ -6,6 +6,18 @@
 # Používateľ zadá zoznam celých čísel. Zabaľte prijaté údaje a uložte ich do súboru.
 # Potom načítajte údaje zo súboru do nového zoznamu.
 
+# verzia1:
+import pickle
+
+numbers = [1, 2, 3, 4, 5]
+
+with open("data.pkl", "wb") as file:
+    pickle.dump(numbers, file)
+
+with open("data.pkl", "rb") as file:
+    loaded = pickle.load(file)
+
+# verzia2:
 import pickle
 
 numbers = []
@@ -27,3 +39,22 @@ with open("data.pkl", "rb") as file:
     loaded = pickle.load(file)
 
 print(loaded)
+
+# verzia3:
+import pickle
+
+numbers = []
+user = input("numbers: ")
+parts = user.split(",")
+
+for part in parts:
+    part = part.strip()
+    if part != "":
+        numbers.append(part)
+
+with open("data.pkl", "wb") as file:
+    pickle.dump(numbers, file)
+
+with open("data.pkl", "rb") as file:
+    loaded = pickle.load(file)
+    print(loaded)
