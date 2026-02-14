6d61198783d33e01e7a20ed0ef22e6b4cb99248a
anita <anita.bacova@gmail.com>
2026-02-14 15:14:00 +0100

    type of integers_pickle

---
+++ 02 list of integers_pickle.py
@@ -0,0 +1,29 @@
+# Task 1
+# The user types in a list of integers. Pack the received data and save it to a file.
+# After that, upload the data from the file to a new list.
+#
+# Úloha 1
+# Používateľ zadá zoznam celých čísel. Zabaľte prijaté údaje a uložte ich do súboru.
+# Potom načítajte údaje zo súboru do nového zoznamu.
+
+import pickle
+
+numbers = []
+
+while True:
+    cislo = int(input("Zadaj cislo: "))
+
+    if cislo == -1:
+        break
+
+    numbers.append(cislo)
+
+
+with open("data.pkl", "wb") as file:
+    pickle.dump(numbers, file)
+
+
+with open("data.pkl", "rb") as file:
+    loaded = pickle.load(file)
+
+print(loaded)
